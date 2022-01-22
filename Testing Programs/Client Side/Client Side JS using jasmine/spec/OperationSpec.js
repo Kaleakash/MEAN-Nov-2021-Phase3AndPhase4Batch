30ff533f940ff1e9ffbbe628e542f70f9cfc9146
akash <akash300383@gmail.com>
2022-01-22 12:53:30 +0530

    done

---
+++ Testing Programs/Client Side/Client Side JS using jasmine/spec/OperationSpec.js
@@ -0,0 +1,11 @@
+// Suite 
+describe("Operation Testing",()=> {
+    // spec 
+    it("Addition Testing ",()=> {
+        let result = add(10,20);
+        // expect function 
+        expect(20).toBe(result);
+    });
+
+})
+
